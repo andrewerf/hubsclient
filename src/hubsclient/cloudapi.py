c72d25f8923a77647fc1d7b0e72691f2016cee29
Misha Klopukh <misha@simvia.ai>
2023-05-14 00:54:29 -0400

    Add API media search functions

--- src/hubsclient/cloudapi.py
+++ src/hubsclient/cloudapi.py
@@ -1,9 +1,73 @@
+from typing import Literal, Callable, Any
 from gql import Client, gql
 from gql.transport.requests import RequestsHTTPTransport
+import requests
+import json
+from .utils import typed_dataclass
+
+
+@typed_dataclass
+class RoomInfo:
+    id: str
+    name: str
+    description: str
+    url: str
+    scene_id: str
+    room_size: int
+    lobby_count: int
+    member_count: int
+    user_data: dict
+    is_public: bool
+    preview_image_url: str
+
+    @classmethod
+    def from_obj(cls, data: dict):
+        if not data.get("type") == "room":
+            return data
+        return cls(
+            id=data.get("id", ""),
+            name=data.get("name", ""),
+            description=data.get("description", ""),
+            url=data.get("url", ""),
+            scene_id=data.get("scene_id", ""),
+            room_size=data.get("room_size") or 0,
+            lobby_count=data.get("lobby_count") or 0,
+            member_count=data.get("member_count") or 0,
+            user_data=data.get("user_data") or {},
+            is_public=data.get("is_public", True),
+            preview_image_url=data.get("images", {}).get("preview", {}).get("url") or "",
+        )
+
+
+@typed_dataclass
+class AvatarInfo:
+    id: str
+    name: str
+    description: str
+    url: str
+    preview_images: dict
+    gltfs: dict
+    attributions: dict
+    allow_remixing: bool
+
+    @classmethod
+    def from_obj(cls, data: dict):
+        if not data.get("type") == "avatar_listing":
+            return data
+        return cls(
+            id=data.get("id", ""),
+            name=data.get("name", ""),
+            description=data.get("description", ""),
+            url=data.get("url", ""),
+            preview_images=dict(data.get("images", {})).get("preview") or {},
+            gltfs=data.get("gltfs") or {},
+            attributions=data.get("attributions") or {},
+            allow_remixing=data.get("allow_remixing", True),
+        )
 
 
 class CloudAPI:
-    def __init__(self, host: str, app_token: str = None, user_token: str = None):
+    def __init__(self, host: str, app_token: str = None, user_token: str = None, user_id: str = None):
         """Hubs Cloud API client.
 
         :param host: The host of the room, e.g. "hubs.mozilla.com"
@@ -21,6 +85,7 @@ class CloudAPI:
         self.user_token = user_token
         if user_token is not None:
             self._gql_user_connect()
+        self.user_id = user_id
 
     def _gql_app_connect(self, app_token: str = None):
         self.app_token = app_token or self.app_token
@@ -34,9 +99,7 @@ class CloudAPI:
             verify=True,
             retries=3,
         )
-        self.gqlapp_client = Client(
-            transport=self.gqlapp_transport, fetch_schema_from_transport=True
-        )
+        self.gqlapp_client = Client(transport=self.gqlapp_transport, fetch_schema_from_transport=True)
 
     def _gql_user_connect(self, user_token: str = None):
         self.user_token = user_token or self.user_token
@@ -50,6 +113,44 @@ class CloudAPI:
             verify=True,
             retries=3,
         )
-        self.gqluser_client = Client(
-            transport=self.gqluser_transport, fetch_schema_from_transport=True
-        )
+        self.gqluser_client = Client(transport=self.gqluser_transport, fetch_schema_from_transport=True)
+
+    def _v1api_query(self, route: str, params: dict = {}, method: Literal["GET", "POST"] = "GET"):
+        headers = {
+            'Accept': 'application/json',
+            'User-Agent': 'HubsClient/0.1.0',
+        }
+        if self.user_token is not None:
+            headers['Authorization'] = f'Bearer {self.user_token}'
+        match method:
+            case "GET":
+                return requests.get(f"https://{self.host}/api/v1/{route}", params=params, headers=headers)
+            case "POST":
+                return requests.post(
+                    f"https://{self.host}/api/v1/{route}",
+                    data=json.dumps(params).encode('utf-8'),
+                    headers=headers & {'Content-Type': 'application/json'},
+                )
+
+    def media_search(
+        self,
+        type: Literal["rooms", "scene_listings", "avatar_listings", "scenes", "avatars", "favorites", "assets"],
+        query: str = None,
+        _parser: Callable[[dict], Any] | None = None,
+        **kwargs,
+    ):
+        entries = []
+        cursor = 1
+        while cursor:
+            resp = self._v1api_query(
+                "media/search", params={"source": type, "q": query, "user": self.user_id, cursor: cursor, **kwargs}
+            ).json(object_hook=_parser)
+            cursor = resp["meta"]["next_cursor"]
+            entries.extend(resp["entries"])
+        return entries
+
+    def get_public_rooms(self):
+        return self.media_search("rooms", filter="public", _parser=RoomInfo.from_obj)
+
+    def get_avatars(self):
+        return self.media_search("avatar_listings", _parser=AvatarInfo.from_obj)
