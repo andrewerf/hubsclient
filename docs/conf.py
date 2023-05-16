d8fb4641dc138f40fe462b2974318eb99e490806
Misha Klopukh <misha@simvia.ai>
2023-05-15 20:56:56 -0400

    Notebook for quickstart

--- docs/conf.py
+++ docs/conf.py
@@ -14,11 +14,13 @@ release = "0.0.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["autodoc2", "myst_parser", "sphinx.ext.napoleon", "sphinx.ext.viewcode"]
+extensions = ["autodoc2", "myst_nb", "sphinx.ext.napoleon", "sphinx.ext.viewcode"]
 
 autodoc2_packages = ["../src/hubsclient"]
 
 autodoc2_skip_module_regexes = [r"hubsclient.__about__"]
+
+nb_execution_mode = "off"
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
